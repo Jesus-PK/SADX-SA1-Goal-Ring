0d2a4b7eb6253c44b268f2f8ba5cff3710c51913
Jesus-PK <134032385+Jesus-PK@users.noreply.github.com>
2024-06-08 03:58:12 +0200

    Added basic files.

---
+++ SA1-Goal-Ring/mod.cpp
@@ -0,0 +1,24 @@
+#include "pch.h"
+
+//	Mod Version: W.I.P
+
+std::string ModPath;
+HelperFunctions HelperFunctionsGlobal;
+
+extern "C"
+{
+	__declspec(dllexport) void __cdecl Init(const char* path, const HelperFunctions& helperFunctions)
+	{
+		ModPath = path;
+		HelperFunctionsGlobal = helperFunctions;
+
+		
+
+		
+		//	Init Mod:
+
+		
+	}
+
+	__declspec(dllexport) ModInfo SADXModInfo = { ModLoaderVer };
+}
--- SA1-Goal-Ring/pch.h
+++ SA1-Goal-Ring/pch.h
@@ -1,8 +1,15 @@
 #ifndef PCH_H
 #define PCH_H
 
-#include "framework.h"
+	#include "framework.h"
 
-#include "SADXModLoader.h"
+	#include "SADXModLoader.h"
+
+	#include "ModelInfo.h"
+
+	#include "mod.h"
+
+	#include "goalring.h"
+	#include "utils.h"
 
 #endif
