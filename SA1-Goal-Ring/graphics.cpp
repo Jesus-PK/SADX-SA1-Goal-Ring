b82b30cce769fcfd12573d238ce46433ebd5104c
Jesus-PK <134032385+Jesus-PK@users.noreply.github.com>
2024-06-08 23:32:10 +0200

    Added JP mission cards, improved organization.

--- SA1-Goal-Ring/graphics.cpp
+++ SA1-Goal-Ring/graphics.cpp
@@ -4,85 +4,90 @@
 
 void MISSIONCARDS_GoalRing()
 {
-	std::string index = ModPath;
+	std::string EN_Index = ModPath;
+	std::string JP_Index = ModPath;
 
 	if (HD_GUI)
 	{
-		index += "\\textures\\MissionCards_HD\\index.txt";
-		const char* HD_Index = index.c_str();
+		EN_Index += "\\textures\\EN_MissionCards_HD\\index.txt";
+		const char* HD_EN_Index = EN_Index.c_str();
 
-		ReplacePVR("MISSION_S_BOX_E", HD_Index);
-		ReplacePVR("MISSION_S_BOX2MIN_E", HD_Index);
-		ReplacePVR("MISSION_S_BOX3MIN_E", HD_Index);
-		ReplacePVR("MISSION_S_BOX4MIN_E", HD_Index);
-		ReplacePVR("MISSION_S_BOX5MIN_E", HD_Index);
-		ReplacePVR("MISSION_S_BOX25MIN_E", HD_Index);
-		ReplacePVR("MISSION_S_BOX45MIN_E", HD_Index);
-		ReplacePVR("MISSION_S_RINGBOX_E", HD_Index);
-		ReplacePVR("MISSION_T_BOX_E", HD_Index);
+		ReplacePVR("MISSION_S_BOX_E", HD_EN_Index);
+		ReplacePVR("MISSION_S_BOX2MIN_E", HD_EN_Index);
+		ReplacePVR("MISSION_S_BOX3MIN_E", HD_EN_Index);
+		ReplacePVR("MISSION_S_BOX4MIN_E", HD_EN_Index);
+		ReplacePVR("MISSION_S_BOX5MIN_E", HD_EN_Index);
+		ReplacePVR("MISSION_S_BOX25MIN_E", HD_EN_Index);
+		ReplacePVR("MISSION_S_BOX45MIN_E", HD_EN_Index);
+		ReplacePVR("MISSION_S_RINGBOX_E", HD_EN_Index);
+		ReplacePVR("MISSION_T_BOX_E", HD_EN_Index);
 
-		/*
-		ReplacePVR("MISSION_S_2MIN", HD_Index);
-		ReplacePVR("MISSION_S_2MINH", HD_Index);
-		ReplacePVR("MISSION_S_3MIN", HD_Index);
-		ReplacePVR("MISSION_S_4MIN", HD_Index);
-		ReplacePVR("MISSION_S_4MINH", HD_Index);
-		ReplacePVR("MISSION_S_5MIN", HD_Index);
-		ReplacePVR("MISSION_S_BOX", HD_Index);
-		ReplacePVR("MISSION_S_BOXMIN", HD_Index);
-		ReplacePVR("MISSION_S_RINGBOX", HD_Index);
-		ReplacePVR("MISSION_T_BOX", HD_Index);
-		*/
+
+		JP_Index += "\\textures\\JP_MissionCards_HD\\index.txt";
+		const char* HD_JP_Index = JP_Index.c_str();
+		
+		ReplacePVR("MISSION_S_2MIN", HD_JP_Index);
+		ReplacePVR("MISSION_S_2MINH", HD_JP_Index);
+		ReplacePVR("MISSION_S_3MIN", HD_JP_Index);
+		ReplacePVR("MISSION_S_4MIN", HD_JP_Index);
+		ReplacePVR("MISSION_S_4MINH", HD_JP_Index);
+		ReplacePVR("MISSION_S_5MIN", HD_JP_Index);
+		ReplacePVR("MISSION_S_BOX", HD_JP_Index);
+		ReplacePVR("MISSION_S_BOXMIN", HD_JP_Index);
+		ReplacePVR("MISSION_S_RINGBOX", HD_JP_Index);
+		ReplacePVR("MISSION_T_BOX", HD_JP_Index);		
 	}
 
 	else
 	{
-		index += "\\textures\\MissionCards_SD\\index.txt";
-		const char* SD_Index = index.c_str();
+		EN_Index += "\\textures\\EN_MissionCards_SD\\index.txt";
+		const char* SD_EN_Index = EN_Index.c_str();
 
-		ReplacePVR("MISSION_S_BOX_E", SD_Index);
-		ReplacePVR("MISSION_S_BOX2MIN_E", SD_Index);
-		ReplacePVR("MISSION_S_BOX3MIN_E", SD_Index);
-		ReplacePVR("MISSION_S_BOX4MIN_E", SD_Index);
-		ReplacePVR("MISSION_S_BOX5MIN_E", SD_Index);
-		ReplacePVR("MISSION_S_BOX25MIN_E", SD_Index);
-		ReplacePVR("MISSION_S_BOX45MIN_E", SD_Index);
-		ReplacePVR("MISSION_S_RINGBOX_E", SD_Index);
-		ReplacePVR("MISSION_T_BOX_E", SD_Index);
+		ReplacePVR("MISSION_S_BOX_E", SD_EN_Index);
+		ReplacePVR("MISSION_S_BOX2MIN_E", SD_EN_Index);
+		ReplacePVR("MISSION_S_BOX3MIN_E", SD_EN_Index);
+		ReplacePVR("MISSION_S_BOX4MIN_E", SD_EN_Index);
+		ReplacePVR("MISSION_S_BOX5MIN_E", SD_EN_Index);
+		ReplacePVR("MISSION_S_BOX25MIN_E", SD_EN_Index);
+		ReplacePVR("MISSION_S_BOX45MIN_E", SD_EN_Index);
+		ReplacePVR("MISSION_S_RINGBOX_E", SD_EN_Index);
+		ReplacePVR("MISSION_T_BOX_E", SD_EN_Index);
 
-		ReplacePVR("MISSION_S_BOX_E_DC", SD_Index);
-		ReplacePVR("MISSION_S_BOX2MIN_E_DC", SD_Index);
-		ReplacePVR("MISSION_S_BOX3MIN_E_DC", SD_Index);
-		ReplacePVR("MISSION_S_BOX4MIN_E_DC", SD_Index);
-		ReplacePVR("MISSION_S_BOX5MIN_E_DC", SD_Index);
-		ReplacePVR("MISSION_S_BOX25MIN_E_DC", SD_Index);
-		ReplacePVR("MISSION_S_BOX45MIN_E_DC", SD_Index);
-		ReplacePVR("MISSION_S_RINGBOX_E_DC", SD_Index);
-		ReplacePVR("MISSION_T_BOX_E_DC", SD_Index);
+		ReplacePVR("MISSION_S_BOX_E_DC", SD_EN_Index);
+		ReplacePVR("MISSION_S_BOX2MIN_E_DC", SD_EN_Index);
+		ReplacePVR("MISSION_S_BOX3MIN_E_DC", SD_EN_Index);
+		ReplacePVR("MISSION_S_BOX4MIN_E_DC", SD_EN_Index);
+		ReplacePVR("MISSION_S_BOX5MIN_E_DC", SD_EN_Index);
+		ReplacePVR("MISSION_S_BOX25MIN_E_DC", SD_EN_Index);
+		ReplacePVR("MISSION_S_BOX45MIN_E_DC", SD_EN_Index);
+		ReplacePVR("MISSION_S_RINGBOX_E_DC", SD_EN_Index);
+		ReplacePVR("MISSION_T_BOX_E_DC", SD_EN_Index);
 
-		/*
-		ReplacePVR("MISSION_S_2MIN", SD_Index);
-		ReplacePVR("MISSION_S_2MINH", SD_Index);
-		ReplacePVR("MISSION_S_3MIN", SD_Index);
-		ReplacePVR("MISSION_S_4MIN", SD_Index);
-		ReplacePVR("MISSION_S_4MINH", SD_Index);
-		ReplacePVR("MISSION_S_5MIN", SD_Index);
-		ReplacePVR("MISSION_S_BOX", SD_Index);
-		ReplacePVR("MISSION_S_BOXMIN", SD_Index);
-		ReplacePVR("MISSION_S_RINGBOX", SD_Index);
-		ReplacePVR("MISSION_T_BOX", SD_Index);
 
-		ReplacePVR("MISSION_S_2MIN_DC", SD_Index);
-		ReplacePVR("MISSION_S_2MINH_DC", SD_Index);
-		ReplacePVR("MISSION_S_3MIN_DC", SD_Index);
-		ReplacePVR("MISSION_S_4MIN_DC", SD_Index);
-		ReplacePVR("MISSION_S_4MINH_DC", SD_Index);
-		ReplacePVR("MISSION_S_5MIN_DC", SD_Index);
-		ReplacePVR("MISSION_S_BOX_DC", SD_Index);
-		ReplacePVR("MISSION_S_BOXMIN_DC", SD_Index);
-		ReplacePVR("MISSION_S_RINGBOX_DC", SD_Index);
-		ReplacePVR("MISSION_T_BOX_DC", SD_Index);
-		*/
+		JP_Index += "\\textures\\JP_MissionCards_SD\\index.txt";
+		const char* SD_JP_Index = JP_Index.c_str();
+
+		ReplacePVR("MISSION_S_2MIN", SD_JP_Index);
+		ReplacePVR("MISSION_S_2MINH", SD_JP_Index);
+		ReplacePVR("MISSION_S_3MIN", SD_JP_Index);
+		ReplacePVR("MISSION_S_4MIN", SD_JP_Index);
+		ReplacePVR("MISSION_S_4MINH", SD_JP_Index);
+		ReplacePVR("MISSION_S_5MIN", SD_JP_Index);
+		ReplacePVR("MISSION_S_BOX", SD_JP_Index);
+		ReplacePVR("MISSION_S_BOXMIN", SD_JP_Index);
+		ReplacePVR("MISSION_S_RINGBOX", SD_JP_Index);
+		ReplacePVR("MISSION_T_BOX", SD_JP_Index);
+
+		ReplacePVR("MISSION_S_2MIN_DC", SD_JP_Index);
+		ReplacePVR("MISSION_S_2MINH_DC", SD_JP_Index);
+		ReplacePVR("MISSION_S_3MIN_DC", SD_JP_Index);
+		ReplacePVR("MISSION_S_4MIN_DC", SD_JP_Index);
+		ReplacePVR("MISSION_S_4MINH_DC", SD_JP_Index);
+		ReplacePVR("MISSION_S_5MIN_DC", SD_JP_Index);
+		ReplacePVR("MISSION_S_BOX_DC", SD_JP_Index);
+		ReplacePVR("MISSION_S_BOXMIN_DC", SD_JP_Index);
+		ReplacePVR("MISSION_S_RINGBOX_DC", SD_JP_Index);
+		ReplacePVR("MISSION_T_BOX_DC", SD_JP_Index);
 	}
 }
 
