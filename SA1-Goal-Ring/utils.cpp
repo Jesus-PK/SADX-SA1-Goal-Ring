3b534e31891d4b3ee2019b36a2cf5c4807aacda0
Jesus-PK <134032385+Jesus-PK@users.noreply.github.com>
2024-06-08 04:10:05 +0200

    Added utils files.

--- SA1-Goal-Ring/utils.cpp
+++ SA1-Goal-Ring/utils.cpp
@@ -1,2 +1,37 @@
 #include "pch.h"
 
+//	Bools and code to check if certain mods or config options are enabled:
+
+bool HD_GUI = false;
+bool DC_Conversion = false;
+
+void CheckActiveMods()
+{
+	HD_GUI = HelperFunctionsGlobal.Mods->find("sadx-hd-gui") != nullptr;
+	DC_Conversion = HelperFunctionsGlobal.Mods->find("sadx-dreamcast-conversion") != nullptr;
+}
+
+
+//	Function for loading Basic Models:
+
+ModelInfo* LoadBasicModel(const char* name)
+{
+    PrintDebug("[SA1-Goal-Ring] Loading basic model: %s... ", name);
+
+    std::string fullPath = "system\\models\\";
+    fullPath = fullPath + name + ".sa1mdl";
+
+    ModelInfo* mdl = new ModelInfo(HelperFunctionsGlobal.GetReplaceablePath(fullPath.c_str()));
+
+    if (mdl->getformat() != ModelFormat_Basic)
+    {
+        PrintDebug("Failed!\n");
+
+        delete mdl;
+        return nullptr;
+    }
+
+    PrintDebug("Done!\n");
+
+    return mdl;
+}
--- SA1-Goal-Ring/utils.h
+++ SA1-Goal-Ring/utils.h
@@ -1,1 +1,19 @@
 #pragma once
+
+//	Macros:
+
+#define ReplaceTEX(pvm, tex_name, folder, png_name, gbix, width, height) HelperFunctionsGlobal.ReplaceTexture(pvm, tex_name, (std::string(ModPath) + "\\textures\\" folder "\\" png_name ".png").c_str(), gbix, width, height);
+#define ReplacePVR(a, b) HelperFunctionsGlobal.ReplaceFile("system\\" a ".PVR", b);
+
+
+//	Bools and code to check if certain mods or config options are enabled:
+
+extern bool HD_GUI;
+extern bool DC_Conversion;
+
+void CheckActiveMods();
+
+
+//	Functions for loading Models and Animations:
+
+ModelInfo* LoadBasicModel(const char* name);
