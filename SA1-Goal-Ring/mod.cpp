cca55918c8e5e79a868676e865bcc7360670023a
Jesus-PK <134032385+Jesus-PK@users.noreply.github.com>
2024-06-11 05:11:54 +0200

    Just more startpos testing.

--- SA1-Goal-Ring/mod.cpp
+++ SA1-Goal-Ring/mod.cpp
@@ -23,8 +23,10 @@ extern "C"
 		
 		//	TESTING PURPOSES - PENDING REMOVAL:
 
-		StartPosition STARTPOS_TreeTops00 = { LevelIDs_SkyDeck, 0, { 885.0f, -330.0f, 5478.0f }, 0x0 };
+		StartPosition STARTPOS_TreeTops00 = { LevelIDs_WindyValley, 2, { 5820.0f, -10700.0f, -735.0f }, 0x0};
+		StartPosition STARTPOS_TreeTops01 = { LevelIDs_SkyDeck, 0, { 885.0f, -330.0f, 5478.0f }, 0x0 };
 		HelperFunctionsGlobal.RegisterStartPosition(Characters_Tails, STARTPOS_TreeTops00);
+		HelperFunctionsGlobal.RegisterStartPosition(Characters_Tails, STARTPOS_TreeTops01);
 	}
 
 	__declspec(dllexport) ModInfo SADXModInfo = { ModLoaderVer };
